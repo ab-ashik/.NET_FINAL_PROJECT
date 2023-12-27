9ae1f26fff7e0f77ff1510cce6b06ad09fd49d99
ab-ashik <abdullahashik2001@gmail.com>
2023-12-27 22:10:58 +0600

    DAL model classes created and dependency added

---
+++ DAL/Models/Customers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Models
+{
+    public class Customers
+    {
+        [Key]
+        public String Uname { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public String Name { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public String Password { get; set; }
+
+
+
+    }
+}
---
+++ DAL/Models/Service.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Models
+{
+    public class Service
+    {
+        [Key]
+        public int Id { get; set; }
+
+
+        [Required]
+        [StringLength(20)]
+        public string Sname { get; set; }
+
+
+
+
+    }
+}
