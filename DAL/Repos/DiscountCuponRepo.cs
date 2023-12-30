2270489fdd373339c954507a3c6d47a096c3939e
Mohammad SHaKiB <mdshakib6477@gmail.com>
2023-12-30 15:50:21 +0600

    Interface IRepo & All models Repo created for CRUD

---
+++ DAL/Repos/DiscountCuponRepo.cs
@@ -0,0 +1,46 @@
+﻿using DAL.Interfaces;
+using DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repos
+{
+    internal class DiscountCuponRepo : Repo, IRepo<DiscountCupon, int, DiscountCupon>
+    {
+        public DiscountCupon Create(DiscountCupon obj)
+        {
+            db.DiscountCupons.Add(obj);
+            if (db.SaveChanges() > 0) return obj;
+            return null;
+        }
+
+        public bool Delete(int id)
+        {
+            var ex = Read(id);
+            db.DiscountCupons.Remove(ex);
+            return db.SaveChanges() > 0;
+        }
+
+        public List<DiscountCupon> Read()
+        {
+            return db.DiscountCupons.ToList();
+        }
+
+        public DiscountCupon Read(int id)
+        {
+            return db.DiscountCupons.Find(id);
+
+        }
+
+        public DiscountCupon Update(DiscountCupon obj)
+        {
+            var ex = Read(obj.CuponID);
+            db.Entry(ex).CurrentValues.SetValues(obj);
+            if (db.SaveChanges() > 0) return obj;
+            return null;
+        }
+    }
+}
---
+++ DAL/Repos/WorkerRepo.cs
@@ -0,0 +1,45 @@
+﻿using DAL.Interfaces;
+using DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repos
+{
+    internal class WorkerRepo : Repo, IRepo<Worker, int, Worker>
+    {
+        public Worker Create(Worker obj)
+        {
+            db.Workers.Add(obj);
+            if (db.SaveChanges() > 0) return obj;
+            return null;
+        }
+
+        public bool Delete(int id)
+        {
+            var ex = Read(id);
+            db.Workers.Remove(ex);
+            return db.SaveChanges() > 0;
+        }
+
+        public List<Worker> Read()
+        {
+            return db.Workers.ToList();
+        }
+
+        public Worker Read(int id)
+        {
+            return db.Workers.Find(id);
+        }
+
+        public Worker Update(Worker obj)
+        {
+            var ex = Read(obj.WorkerID);
+            db.Entry(ex).CurrentValues.SetValues(obj);
+            if (db.SaveChanges() > 0) return obj;
+            return null;
+        }
+    }
+}
