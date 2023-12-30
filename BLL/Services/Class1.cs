0dd324ff36d4a28dc9ff3ec9bd03685231f9cc4e
ab-ashik <abdullahashik2001@gmail.com>
2023-12-31 03:11:16 +0600

    for merging

--- BLL/Services/Class1.cs
+++ BLL/Services/Class1.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BLL.Services
+﻿namespace BLL.Services
 {
     internal class Class1
     {
