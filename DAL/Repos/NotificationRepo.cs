3cb538aed3c17a81194a40b81d73c9aa2d61dbb2
Mohammad SHaKiB <mdshakib6477@gmail.com>
2023-12-30 16:13:38 +0600

    Interface IRepo & all Models Repo are created for CRUD

---
+++ DAL/Repos/NotificationRepo.cs
@@ -0,0 +1,45 @@
+﻿using DAL.Interfaces;
+using DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repos
+{
+    internal class NotificationRepo : Repo, IRepo<Notification, int, Notification>
+    {
+        public Notification Create(Notification obj)
+        {
+            db.Notifications.Add(obj);
+            if (db.SaveChanges() > 0) return obj;
+            return null;
+        }
+
+        public bool Delete(int id)
+        {
+            var ex = Read(id);
+            db.Notifications.Remove(ex);
+            return db.SaveChanges() > 0;
+        }
+
+        public List<Notification> Read()
+        {
+            return db.Notifications.ToList();
+        }
+
+        public Notification Read(int id)
+        {
+            return db.Notifications.Find(id);
+        }
+
+        public Notification Update(Notification obj)
+        {
+            var ex = Read(obj.NotificationID);
+            db.Entry(ex).CurrentValues.SetValues(obj);
+            if (db.SaveChanges() > 0) return obj;
+            return null;
+        }
+    }
+}
---
+++ DAL/Repos/ServiceHistoryRepo.cs
@@ -0,0 +1,45 @@
+﻿using DAL.Interfaces;
+using DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repos
+{
+    internal class ServiceHistoryRepo : Repo, IRepo<ServiceHistory, int, ServiceHistory>
+    {
+        public ServiceHistory Create(ServiceHistory obj)
+        {
+            db.ServicesHistories.Add(obj);
+            if (db.SaveChanges() > 0) return obj;
+            return null;
+        }
+
+        public bool Delete(int id)
+        {
+            var ex = Read(id);
+            db.ServicesHistories.Remove(ex);
+            return db.SaveChanges() > 0;
+        }
+
+        public List<ServiceHistory> Read()
+        {
+            return db.ServicesHistories.ToList();
+        }
+
+        public ServiceHistory Read(int id)
+        {
+            return db.ServicesHistories.Find(id);
+        }
+
+        public ServiceHistory Update(ServiceHistory obj)
+        {
+            var ex = Read(obj.HistoryID);
+            db.Entry(ex).CurrentValues.SetValues(obj);
+            if (db.SaveChanges() > 0) return obj;
+            return null;
+        }
+    }
+}
