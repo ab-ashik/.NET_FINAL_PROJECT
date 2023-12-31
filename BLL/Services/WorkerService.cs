cb47f0e07f5cd43796025cbba6f6e69a9247b85c
Mohammad SHaKiB <mdshakib6477@gmail.com>
2023-12-31 14:17:53 +0600

    WorkerService and WorkerController Modified

---
+++ BLL/Services/WorkerService.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using BLL.DTOs;
+using DAL;
+using DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    public class WorkerService
+    {
+        public class PostService
+        {
+            public static List<WorkerDTO> Get()
+            {
+                var data = DataAccessFactory.WorkerData().Read();
+                var cfg = new MapperConfiguration(c =>
+                {
+                    c.CreateMap<Worker, WorkerDTO>();
+                });
+                var mapper = new Mapper(cfg);
+                var mapped = mapper.Map<List<WorkerDTO>>(data);
+                return mapped;
+
+            }
+            public static WorkerDTO Get(int id)
+            {
+                var data = DataAccessFactory.WorkerData().Read(id);
+                var cfg = new MapperConfiguration(c =>
+                {
+                    c.CreateMap<Worker, WorkerDTO>();
+                });
+                var mapper = new Mapper(cfg);
+                var mapped = mapper.Map<WorkerDTO>(data);
+                return mapped;
+
+            }
+        }
+    }
+}
