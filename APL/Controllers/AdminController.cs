35dcf96e69290957e1cd1da3770bb198e4aef0d6
Rafid-Rahman <rafidrahman00@gmail.com>
2024-01-02 00:31:43 +0600

    Booking List Added for Admin

---
+++ APL/Controllers/AdminController.cs
@@ -0,0 +1,29 @@
+﻿using BLL.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace APL.Controllers
+{
+    public class AdminController : ApiController
+    {
+        [HttpGet]
+        [Route("api/admin/bookings/all")]
+        public HttpResponseMessage Bookings()
+        {
+            try
+            {
+                var data = AdminService.GetAllBooking();
+                return Request.CreateResponse(HttpStatusCode.OK, data);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, new { Message = ex.Message });
+            }
+        }
+
+    }
+}
